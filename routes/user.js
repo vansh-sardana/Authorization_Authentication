744d41d537e2fc6bc9095e9b998c207a56fabce1
Vansh Sardana <vanshsrdn25703@gmail.com>
2024-03-24 17:58:48 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const express= require("express");
+const router= express.Router();
+
+
+
+const {login, signup}= require("../controllers/Auth");
+const { auth, isStudent, isAdmin } = require("../middlewares/auth");
+
+router.post("/login",login);
+router.post("/signup", signup);
+
+router.get("/admin", auth, isAdmin, (req, res)=>{
+    res.status(200).json({
+        success: true,
+        message: "Welcome to the Admin Page"
+    })
+})
+
+router.get("/student", auth, isStudent, (req, res)=>{
+    res.status(200).json({
+        success: true,
+        message: "Welcome to the students Page"
+    })
+})
+
+module.exports = router;
